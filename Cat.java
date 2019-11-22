fbbb76c4833219b937aa429af5f72a8e72256158
MorganNkosi <54417130+MorganNkosi@users.noreply.github.com>
2019-11-22 16:51:24 +0200

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,9 @@
+package com.animaloop.java;
+
+public class Cat extends Animal {
+
+    private static String name = "Storm";
+    public void sounds(){
+        System.out.println("Cat meows");
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,10 @@
+package com.animaloop.java;
+
+public class Dog extends Animal {
+
+    private static String name = "Rax";
+
+    public void sounds() {
+        System.out.println("Dog barks");
+    }
+}
